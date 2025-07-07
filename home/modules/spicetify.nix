98f9a41d802d19703a03ddc4592b838f693b5432
David Thach <davidthach@live.com>
2025-07-07 09:12:43 -0400

    Simplify spicetify config - remove extensions for now, focus on Stylix theming

--- home/modules/spicetify.nix
+++ home/modules/spicetify.nix
@@ -6,19 +6,5 @@
     
     # Let Stylix handle the theming automatically
     # The theme and colorScheme will be overridden by Stylix
-    
-    # Enable common extensions
-    enabledExtensions = with config.programs.spicetify.extensions; [
-      adblock
-      hidePodcasts
-      shuffle
-      keyboardShortcut
-    ];
-    
-    # Enable common custom apps
-    enabledCustomApps = with config.programs.spicetify.customApps; [
-      newReleases
-      betterLibrary
-    ];
   };
 }
