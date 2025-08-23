24372577cd626d0fa4b9695d74933de04555c3f4
David Thach <davidthach@live.com>
2025-08-23 16:45:43 -0400

    Revert problematic boot and greetd configurations
    - Remove aggressive kernel boot message suppression that was hiding critical errors
    - Remove systemd initrd configuration that could cause boot failures
    - Remove complex greetd TTY configurations that conflicted with boot process
    - Simplify greetd configuration to basic working state

--- core/modules/greetd.nix
+++ core/modules/greetd.nix
@@ -15,37 +15,10 @@
           "--asterisks-char ●"
           "--window-padding 2"
           "--greeting 'Welcome to NixOS'"
-          "--remember"
-          "--remember-user-session"
-          "--power-shutdown 'systemctl poweroff'"
-          "--power-reboot 'systemctl reboot'"
           #"--theme 'border=magenta;text=text;prompt=sky;time=overlay1;action=blue;button=surface0;container=base;input=surface0'"
         ];
       };
     };
   };
 
-  # Additional systemd configuration to prevent console messages from interfering
-  systemd.services.greetd = {
-    unitConfig = {
-      After = "systemd-user-sessions.service plymouth-quit-wait.service getty@tty1.service";
-    };
-    serviceConfig = {
-      # Clear the console before starting greetd
-      ExecStartPre = "${pkgs.coreutils}/bin/clear";
-      # Ensure greetd has exclusive access to the console
-      TTYPath = "/dev/tty1";
-      TTYReset = "yes";
-      TTYVHangup = "yes";
-      TTYVTDisallocate = "yes";
-      # Prevent other processes from writing to the console
-      StandardInput = "tty-force";
-      StandardOutput = "tty";
-      StandardError = "tty";
-    };
-  };
-
-  # Disable getty on tty1 since greetd will use it
-  systemd.services."getty@tty1".enable = false;
-  systemd.services."autovt@tty1".enable = false;
 }
