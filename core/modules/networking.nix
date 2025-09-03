4bbf1857ef83bb67594f2f118e83a279f68429a9
David Thach <davidthach@live.com>
2025-09-03 17:28:36 -0400

    Fix Chromium module: move system configs to networking module

--- core/modules/networking.nix
+++ core/modules/networking.nix
@@ -8,7 +8,15 @@
       allowPing = true;
 
       # Open ports as needed
-      allowedTCPPorts = [ 22 ];
+      allowedTCPPorts = [ 22 8008 8009 ]; # 8008/8009 for Chromecast
+      allowedUDPPorts = [ 5353 ]; # mDNS for device discovery
     };
+  };
+
+  # Enable Avahi for mDNS/DNS-SD service discovery (needed for Chromecast)
+  services.avahi = {
+    enable = true;
+    nssmdns4 = true;
+    openFirewall = true;
   };
 }
--- home/modules/chromium.nix
+++ home/modules/chromium.nix
@@ -1,21 +1,8 @@
 { config, pkgs, ... }: {
-  # Chromium package with policy configuration
+  # Chromium package
   home.packages = with pkgs; [
     chromium
   ];
-
-  # Enable Avahi for mDNS/DNS-SD service discovery (needed for Chromecast)
-  services.avahi = {
-    enable = true;
-    nssmdns4 = true;
-    openFirewall = true;
-  };
-
-  # Open firewall ports for Chromecast
-  networking.firewall = {
-    allowedTCPPorts = [ 8008 8009 ]; # Chromecast communication ports
-    allowedUDPPorts = [ 5353 ]; # mDNS for device discovery
-  };
 
   # Configure Chromium to not ask about default browser
   home.file.".config/chromium/Default/Preferences" = {
