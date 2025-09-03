77d981fd80b6a3d82c00ee117e4e6b0dd29ceada
David Thach <davidthach@live.com>
2025-09-03 17:33:38 -0400

    Use programs.chromium with proper command line args and policies

--- home/modules/chromium.nix
+++ home/modules/chromium.nix
@@ -1,23 +1,20 @@
-{ config, pkgs, ... }: 
-let
-  chromium-wrapped = pkgs.chromium.override {
+{ config, pkgs, ... }: {
+  programs.chromium = {
+    enable = true;
     commandLineArgs = [
       "--no-default-browser-check"
       "--disable-default-apps"
+      "--no-first-run"
     ];
   };
-in {
-  # Chromium package with no default browser check
-  home.packages = with pkgs; [
-    chromium-wrapped
-  ];
 
   # Set policy files to disable default browser prompts
   home.file.".config/chromium/policies/managed/no_default_browser_check.json" = {
     text = ''
       {
         "DefaultBrowserSettingEnabled": false,
-        "BrowserAddPersonEnabled": false
+        "BrowserAddPersonEnabled": false,
+        "DefaultBrowserPromptEnabled": false
       }
     '';
   };
