459ccc8d4f4b80bd22741c255d4f05bccd503279
David Thach <davidthach@live.com>
2025-10-04 15:36:07 -0400

    Fix catppuccin SDDM theme name

--- core/modules/sddm.nix
+++ core/modules/sddm.nix
@@ -3,12 +3,15 @@
   services.displayManager.sddm = {
     enable = true;
     wayland.enable = true;
-    theme = "catppuccin-mocha";
+    theme = "catppuccin-mocha-mauve";
     package = pkgs.kdePackages.sddm;
   };
 
   environment.systemPackages = [
-    pkgs.catppuccin-sddm
+    (pkgs.catppuccin-sddm.override {
+      flavor = "mocha";
+      accent = "mauve";
+    })
   ];
 
 }
