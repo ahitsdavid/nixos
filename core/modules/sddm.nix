516c46f7d2b6bf6517e11c3d2bbe5cf868dd2976
David Thach <davidthach@live.com>
2025-10-04 15:33:02 -0400

    Add catppuccin-sddm theme

--- core/modules/sddm.nix
+++ core/modules/sddm.nix
@@ -3,6 +3,12 @@
   services.displayManager.sddm = {
     enable = true;
     wayland.enable = true;
+    theme = "catppuccin-mocha";
+    package = pkgs.kdePackages.sddm;
   };
+
+  environment.systemPackages = [
+    pkgs.catppuccin-sddm
+  ];
 
 }
