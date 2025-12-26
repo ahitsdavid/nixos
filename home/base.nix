4cc6a041508d34dc33975409fbfe70c1b7247b70
David Thach <davidthach@live.com>
2025-12-26 15:43:43 -0500

    Set Firefox as default browser for HTTP/HTTPS URLs

--- home/base.nix
+++ home/base.nix
@@ -60,5 +60,17 @@
     };
   };
 
+  # Set Firefox as default browser for all web links
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications = {
+      "text/html" = "firefox.desktop";
+      "x-scheme-handler/http" = "firefox.desktop";
+      "x-scheme-handler/https" = "firefox.desktop";
+      "x-scheme-handler/about" = "firefox.desktop";
+      "x-scheme-handler/unknown" = "firefox.desktop";
+    };
+  };
+
   home.stateVersion = "25.11";
 }
