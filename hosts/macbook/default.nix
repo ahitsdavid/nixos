5626dcab88844f8e3991bcacb21479ab5171adbd
David Thach <davidthach@live.com>
2026-01-28 20:39:08 -0500

    Added gnome plugins/may remove hyprland from macbook (so slow)

--- hosts/macbook/default.nix
+++ hosts/macbook/default.nix
@@ -74,6 +74,18 @@
   # Firmware updates
   services.fwupd.enable = true;
 
+  # GNOME Desktop Environment (optional, selectable at login)
+  services.xserver.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
+  # Exclude some GNOME default apps to keep it lighter
+  environment.gnome.excludePackages = with pkgs; [
+    gnome-tour
+    epiphany  # GNOME Web browser
+    geary     # Email client
+    gnome-music
+  ];
+
   environment.systemPackages = with pkgs; [
     # Monitoring tools
     lm_sensors
@@ -85,6 +97,16 @@
     # Graphics
     mesa
     mesa-demos
+
+    # GNOME tweaks and extensions
+    gnome-tweaks
+    gnome-extension-manager
+    gnomeExtensions.caffeine           # Prevent screen sleep
+    gnomeExtensions.appindicator       # System tray support
+    gnomeExtensions.dash-to-dock       # Better dock
+    gnomeExtensions.clipboard-indicator # Clipboard history
+    gnomeExtensions.blur-my-shell      # Visual blur effects
+    gnomeExtensions.vitals             # System monitor in top bar
   ];
 
   networking = {
