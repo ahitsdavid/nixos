47ce7eeec031d7a4c72822baf271595f183a6316
David Thach <davidthach@live.com>
2026-02-15 00:45:19 -0500

    -starship update

--- home/modules/starship.nix
+++ home/modules/starship.nix
@@ -1,5 +1,6 @@
 # home/modules/starship.nix
-# Starship prompt - enabled for Fish only (Zsh uses powerlevel10k)
+# Starship prompt - Catppuccin Mocha powerline
+# Adapted from: https://starship.rs/presets/catppuccin-powerline
 { config, pkgs, lib, ... }:
 
 {
@@ -11,27 +12,29 @@
     enableFishIntegration = true;
     enableBashIntegration = false;
 
-    # Catppuccin colors handled by catppuccin.nix module
-    # We just define the prompt format here
-
     settings = {
-      # Prompt format
+      # Catppuccin Mocha gradient: blue -> mauve -> pink -> flamingo -> rosewater -> lavender
       format = lib.concatStrings [
-        "[](blue)"
+        "[](blue)"
         "$os"
         "$username"
-        "[](bg:sapphire fg:blue)"
+        "[](bg:mauve fg:blue)"
         "$directory"
-        "[](fg:sapphire bg:surface1)"
+        "[](bg:pink fg:mauve)"
         "$git_branch"
         "$git_status"
-        "[](fg:surface1 bg:surface0)"
-        "$python"
-        "$nodejs"
+        "[](fg:pink bg:flamingo)"
+        "$c"
         "$rust"
         "$golang"
+        "$nodejs"
+        "$python"
         "$nix_shell"
-        "[](fg:surface0)"
+        "[](fg:flamingo bg:rosewater)"
+        "$docker_context"
+        "[](fg:rosewater bg:lavender)"
+        "$time"
+        "[ ](fg:lavender)"
         "$fill"
         "$cmd_duration"
         "$line_break"
@@ -41,75 +44,97 @@
       # OS icon
       os = {
         disabled = false;
-        style = "bg:blue fg:base";
-        symbols.NixOS = " ";
+        style = "bg:blue fg:crust";
+        symbols.NixOS = " ";
       };
 
       # Username
       username = {
         show_always = true;
-        style_user = "bg:blue fg:base";
-        style_root = "bg:red fg:base";
+        style_user = "bg:blue fg:crust";
+        style_root = "bg:blue fg:crust";
         format = "[ $user ]($style)";
       };
 
-      # Directory
+      # Directory - fish-style abbreviation
       directory = {
-        style = "bg:sapphire fg:base";
+        style = "bg:mauve fg:crust";
         format = "[ $path ]($style)";
         truncation_length = 3;
-        truncation_symbol = ".../";
+        truncation_symbol = "…/";
+        fish_style_pwd_dir_length = 1;
         substitutions = {
           Documents = "󰈙 ";
-          Downloads = " ";
-          Music = " ";
-          Pictures = " ";
-          nixos = " ";
+          Downloads = " ";
+          Music = " ";
+          Pictures = " ";
+          Developer = " ";
         };
       };
 
       # Git
       git_branch = {
-        symbol = "";
-        style = "bg:surface1 fg:text";
-        format = "[ $symbol $branch ]($style)";
+        symbol = "";
+        style = "bg:pink";
+        format = "[[ $symbol $branch ](fg:crust bg:pink)]($style)";
       };
 
       git_status = {
-        style = "bg:surface1 fg:text";
-        format = "[$all_status$ahead_behind ]($style)";
+        style = "bg:pink";
+        format = "[[($all_status$ahead_behind )](fg:crust bg:pink)]($style)";
       };
 
       # Languages
-      python = {
-        symbol = "";
-        style = "bg:surface0 fg:yellow";
-        format = "[ $symbol ($version) ]($style)";
-      };
-
-      nodejs = {
-        symbol = "";
-        style = "bg:surface0 fg:green";
-        format = "[ $symbol ($version) ]($style)";
+      c = {
+        symbol = " ";
+        style = "bg:flamingo";
+        format = "[[ $symbol( $version) ](fg:crust bg:flamingo)]($style)";
       };
 
       rust = {
-        symbol = "";
-        style = "bg:surface0 fg:peach";
-        format = "[ $symbol ($version) ]($style)";
+        symbol = "";
+        style = "bg:flamingo";
+        format = "[[ $symbol( $version) ](fg:crust bg:flamingo)]($style)";
       };
 
       golang = {
-        symbol = "";
-        style = "bg:surface0 fg:sky";
-        format = "[ $symbol ($version) ]($style)";
+        symbol = "";
+        style = "bg:flamingo";
+        format = "[[ $symbol( $version) ](fg:crust bg:flamingo)]($style)";
+      };
+
+      nodejs = {
+        symbol = "";
+        style = "bg:flamingo";
+        format = "[[ $symbol( $version) ](fg:crust bg:flamingo)]($style)";
+      };
+
+      python = {
+        symbol = "";
+        style = "bg:flamingo";
+        format = "[[ $symbol( $version) ](fg:crust bg:flamingo)]($style)";
       };
 
       # Nix shell indicator
       nix_shell = {
-        symbol = "";
-        style = "bg:surface0 fg:blue";
-        format = "[ $symbol $state ]($style)";
+        symbol = "";
+        style = "bg:flamingo";
+        format = "[[ $symbol $state ](fg:crust bg:flamingo)]($style)";
+      };
+
+      # Docker context
+      docker_context = {
+        symbol = "";
+        style = "bg:rosewater";
+        format = "[[ $symbol( $context) ](fg:crust bg:rosewater)]($style)";
+      };
+
+      # Time
+      time = {
+        disabled = false;
+        time_format = "%R";
+        style = "bg:lavender";
+        format = "[[  $time ](fg:crust bg:lavender)]($style)";
       };
 
       # Right side fill
@@ -126,17 +151,18 @@
 
       # Prompt character
       character = {
-        success_symbol = "[❯](bold green)";
-        error_symbol = "[❯](bold red)";
+        success_symbol = "[❯](bold fg:green)";
+        error_symbol = "[❯](bold fg:red)";
+        vimcmd_symbol = "[❮](bold fg:green)";
       };
 
-      # Line break between segments and input
+      # Two-line prompt
       line_break = {
         disabled = false;
       };
     };
   };
 
-  # Let Catppuccin module handle starship theming
+  # Catppuccin module provides the Mocha palette
   catppuccin.starship.enable = true;
 }
